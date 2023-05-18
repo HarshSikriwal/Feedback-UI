a4b7c7fd0ed2662ebd978b62abb776b94f488a77
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-05-18 13:38:54 +0530

    MockBackEnd added

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,15 @@
+import spinner from'../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <img 
+    src={spinner} 
+    alt='Loading..' 
+    style={{
+      width:'100px', margin: 'auto', display:'block', padding:'20px'
+    }}
+    />
+  )
+}
+
+export default Spinner
